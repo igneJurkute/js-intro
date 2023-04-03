0500e4e510cd9d722d2777d0b6b95ab169d3722f
igneJurkute <127085677+igneJurkute@users.noreply.github.com>
2023-04-03 15:55:14 +0300

    boolean, array, typeof

---
+++ js/03-boolean.js
@@ -0,0 +1,15 @@
+console.clear();
+
+/*
+BOOLEAN - logine reiksme
+values: true, false.
+false   -> '', 0
+true    -> 'a', -1, 1
+*/
+
+const arSvieciaSaule = true;
+const arSvieciaMenulis = false;
+const arMenulisAtspindiSviesa = true;
+
+const isValid = true;
+const isLoggedIn = false;
